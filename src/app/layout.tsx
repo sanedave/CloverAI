d6eee4d69c51c282898d599596575ec44ef72f7b
sanedave <studyroom963@gmail.com>
2025-06-04 22:46:02 -0700

    Updated layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
-
 import type {Metadata} from 'next';
-import {Geist, Geist_Mono} from 'next/font/google';
+import { GeistSans as Geist, GeistMono as Geist_Mono } from '@vercel/geist/font';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
